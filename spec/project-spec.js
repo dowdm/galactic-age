257e24914617651aa8060cd293d32a863e990adc
Matt Dowdney <theofficialmatt@gmail.com>
2018-04-20 10:42:54 -0700

    adds new variable for comparing birthday and establishes second test

--- spec/project-spec.js
+++ spec/project-spec.js
@@ -2,16 +2,21 @@ import { DateCompare } from "./../src/class.js";
 
 describe('DateCompare', function() {
   let twoDates1;
+  let twoDates2;
 
   beforeEach(function() {
     const inputDate1 = new Date(1983, 0, 31);
     const inputDate2 = new Date(2018, 3, 20);
     const inputDate3 = new Date(2018, 3, 19);
     const geezerDate = new Date(1933, 0, 31);
-    twoDates1 = new DateCompare(inputDate2, inputDate3)
+    twoDates1 = new DateCompare(inputDate2, inputDate3);
+    twoDates2 = new DateCompare(inputDate2, inputDate1);
   });
 
   it('should return the difference in seconds of both dates', function() {
     expect(twoDates1.getDifference()).toEqual(86400);
+  });
+  it('should return the difference in seconds of both dates', function() {
+    expect(twoDates2.getDifference()).toEqual(1,111,363,200);
   });
 });
