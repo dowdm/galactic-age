3a3c6eb1bd08e9328f46a968e7d7efcda7876222
Matt Dowdney <theofficialmatt@gmail.com>
2018-04-20 10:33:31 -0700

    adds super() to object constructor and divides utc  ms time by 1000

--- src/class.js
+++ src/class.js
@@ -1,12 +1,13 @@
 class DateCompare extends Date {
   constructor(date1, date2) {
+    super();
     this.date1 = date1;
     this.date2 = date2;
 
   }
 
   getDifference() {
-    let difference = Math.abs(this.date1 - this.date2);
+    let difference = Math.abs(this.date1/1000 - this.date2/1000);
     return difference;
   }
 }
