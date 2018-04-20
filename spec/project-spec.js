e5c9159b962268f6070b84ef9c0e0ec37b22068f
Matt Dowdney <theofficialmatt@gmail.com>
2018-04-20 10:36:17 -0700

    moves variables to proper declaration areas, passes first test

--- spec/project-spec.js
+++ spec/project-spec.js
@@ -1,21 +1,17 @@
 import { DateCompare } from "./../src/class.js";
 
-describe('Class', function() {
-  let inputDate1;
-  let inputDate2;
-  let inputDate2;
-  let geezerDate;
-  let twoDates;
+describe('DateCompare', function() {
+  let twoDates1;
 
   beforeEach(function() {
-    inputDate1 = new Date(1983, 0, 31);
-    inputDate2 = new Date(2018, 3, 20);
-    inputDate3 = new Date(2018, 3, 19);
-    geezerDate = new Date(1933, 0, 31);
+    const inputDate1 = new Date(1983, 0, 31);
+    const inputDate2 = new Date(2018, 3, 20);
+    const inputDate3 = new Date(2018, 3, 19);
+    const geezerDate = new Date(1933, 0, 31);
     twoDates1 = new DateCompare(inputDate2, inputDate3)
   });
 
   it('should return the difference in seconds of both dates', function() {
-    expect(twoDates1.getDifference).toEqual(86400);
+    expect(twoDates1.getDifference()).toEqual(86400);
   });
 });
