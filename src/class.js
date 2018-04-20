2a7578fcc66aed86690be5efb309eff7fcce80bf
Matt Dowdney <theofficialmatt@gmail.com>
2018-04-20 11:31:10 -0700

    adds toMercury method to calculate date Mercurial solar age

--- src/class.js
+++ src/class.js
@@ -10,6 +10,12 @@ class DateCompare extends Date {
     let difference = Math.abs(this.date1/1000 - this.date2/1000);
     return difference;
   }
+  toMercury() {
+    let difference = Math.abs(this.date1/1000 - this.date2/1000);
+    let years = difference/31536000;
+    let mercury= years/.24;
+    return mercury
+  }
 }
 
 export { DateCompare };
