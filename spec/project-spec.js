1c2416de9b7b921c6ae57e432e885690536be425
Matt Dowdney <theofficialmatt@gmail.com>
2018-04-20 14:03:58 -0700

    add test for exceeded years on mercury

--- spec/project-spec.js
+++ spec/project-spec.js
@@ -3,6 +3,7 @@ import { DateCompare } from "./../src/class.js";
 describe('DateCompare', function() {
   let twoDates1;
   let twoDates2;
+  let twoDates3;
 
   beforeEach(function() {
     const inputDate1 = new Date(1983, 0, 31);
@@ -11,6 +12,7 @@ describe('DateCompare', function() {
     const geezerDate = new Date(1933, 0, 31);
     twoDates1 = new DateCompare(inputDate2, inputDate3);
     twoDates2 = new DateCompare(inputDate2, inputDate1);
+    twoDates3 = new DateCompare(inputDate2, geezerDate)
   });
 
   it('should return the difference in seconds of both dates', function() {
@@ -38,19 +40,22 @@ describe('DateCompare', function() {
     expect(twoDates2.toJupiter()[0]).toEqual(2);
   });
 
-  it('should return the Mercurial solar year age of given dates', function() {
-    expect(twoDates2.toMercury()[1]).toEqual(186);
+  it('should return the Mercurial solar year expectancy remaining of given dates', function() {
+    expect(twoDates2.toMercury()[1]).toEqual('186 left');
   });
 
-  it('should return the Mercurial solar year age of given dates', function() {
+  it('should return the Venerian solar year expectancy remaining of given dates', function() {
     expect(twoDates2.toVenus()[1]).toEqual(72);
   });
 
-  it('should return the Mercurial solar year age of given dates', function() {
+  it('should return the Martian solar year expectancy remaining of given dates', function() {
     expect(twoDates2.toMars()[1]).toEqual(23);
   });
 
-  it('should return the Mercurial solar year age of given dates', function() {
+  it('should return the Jovian solar year expectancy remaining of given dates', function() {
     expect(twoDates2.toJupiter()[1]).toEqual(3);
+  });
+  it('should return the Mercury solar year life expectancy exceeded of given age', function() {
+    expect(twoDates3.toMercury()[1]).toEqual(3);
   });
 });
